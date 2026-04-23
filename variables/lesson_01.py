4c670022836fc9cc83b1f3da36e78e23910b6ac7
Yana Lepska <lepskayana1624@gmail.com>
2026-04-23 11:59:20 +0300

    Exercises 1–10. Variables and data types.

---
+++ variables/lesson_01.py
@@ -0,0 +1,68 @@
+# Task 1
+# # a = input("Enter a number A: ")
+# b = input("Enter a number B: ")
+# c = input("Enter a number C: ") 
+
+# a = int(a)
+# b = int(b)
+# c = int(c)
+
+# print("The sum is: ", a + b + c)
+# print("The product is: ", a * b * c)
+# print("The pow is:", a**(b-c))
+
+# Task 2
+# n = input("Enter a number: ")
+# result = int(n) + int((n+n)) + int((n+n+n))
+# print(result )
+
+# Task 3
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# age = int(age)
+# hobby = input("Enter your hobby: ")
+# print("Hello, my name is " + name + "! I am " + str(age) + " years old and my hobby is " + hobby + ".")
+
+# Task 4
+# a = input("Enter a length A: ")
+# b = input("Enter a length B: ")
+# a = int(a)
+# b = int(b)
+# S = (a * b) / 2
+# print("The area of the triangle is: ", float(S))
+
+# Task 5
+# number = input("Enter a number: ")
+# number = int(number)
+# next_number = number + 1
+# previous_number = number - 1
+# print("The next number for the number " + str(number) + " is " + str(next_number) + ".")
+# print("The previous number for the number " + str(number) + " is " + str(previous_number) + ".")
+
+# Task 6
+# number = input("Enter a number: ")
+# number_arr = list(number)
+# print(number_arr[-1])
+
+# Task 7
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# print("Hello,"+ " " + surname + " " + name + "! Nice to meet you!") 
+
+# Task 8
+# name= input("Enter your name: ")
+# print("Hello, ", name, end="!")
+
+# Task 9
+# number = input("Enter a number: ")
+# number = int(number)
+# if number <= 100 and number >= 0:
+#     print(2**number)
+
+# Task 10
+# number = input("Enter a number: ")
+# number = int(number)
+# result = number/10
+# print("The result is: ", int(result))
+
+# python variables/lesson_01.py

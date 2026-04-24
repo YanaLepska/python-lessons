db1b735b4653ed2c07c542c9f59d7883403d565c
Yana Lepska <lepskayana1624@gmail.com>
2026-04-24 15:15:07 +0300

    Create lesson_03.py
    Exercises 20–30. Variables and data types.

---
+++ variables/lesson_03.py
@@ -0,0 +1,73 @@
+from math import sqrt
+# Task 1
+# a = input("Enter a number A: ")
+# a = int(a)
+# print(f"{a:05d}")
+
+# Task 2
+# a = input("Enter a number A: ")
+# a = int(a)
+# print(f"{a:*<7d}")
+
+# Task 3
+# a = input("Enter a number A: ")
+# a = float(a)
+# percent = a * 100
+# print(f"{percent:.2f}%")
+
+# Task 4
+# day = input("Enter a day: ")
+# hour = input("Enter an hour: ")
+# minute = input("Enter a minute: ")
+# second = input("Enter a second: ")
+# day = int(day)
+# hour = int(hour)    
+# minute = int(minute)
+# second = int(second)
+# total_seconds = (day * 24 * 3600) + (hour * 3600) + (minute * 60) + second
+# print(f"The total number of seconds is: {total_seconds}")
+
+# Task 5
+# a = input("Enter the desired future value:")
+# b = input("Enter the annual interest rate: ")
+# c = input("Enter the number of years: ")
+# a = float(a)
+# b = float(b)
+# c = int(c)
+# future_value = a / (1 + b)**c
+# print(f"The future value of the investment is: {future_value:.2f}")
+
+# Task 6
+# a = input("Enter a number: ")
+# result = int(a) +(int(a+a))+(int(a+a+a))+(int(a+a+a+a))
+# print(result)
+
+# Task 7
+# a= input("Enter a number: ")
+# a = int(a)
+# b = input("Enter another number: ")
+# b = int(b)
+# c = input("Enter one more number: ")
+# c = int(c)
+# print(f"{c} {b} {a} ")
+
+# Task 8
+# a= input("Enter a number: ")
+# a = int(a)
+# result = a**181
+# print(result)
+
+# Task 9
+# a = 3
+# b = 4
+# c=sqrt(a**2 + b**2)
+# print(f"The length of circle is: {c:.4f}")
+
+#Task 10
+# c=5
+# p = (a + b + c) / 2
+# S = sqrt(p * (p - a) * (p - b) * (p - c))
+# print(f"The area of triangle is: {S:.2f}")
+
+
+# python variables/lesson_03.py

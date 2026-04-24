e95e3d0c9723637546104df46e0a208c746d9708
Yana Lepska <lepskayana1624@gmail.com>
2026-04-24 11:09:52 +0300

    Exercises 10–20. Variables and data types.

---
+++ variables/lesson_02.py
@@ -0,0 +1,73 @@
+# Task 1
+# a = input("Enter a number A: ")
+# a = int(a)
+# print(f"{a:,}")
+
+# Task 2
+# radius = input("Enter the radius of the circle: ")
+# radius = int(radius)
+# pi = 3.14
+# area = pi * radius**2
+# length = 2 * pi * radius
+# print (f"The area of circle is: {area:.3f}")
+# print (f"The length of circle is: {length:.3f}")
+
+# Task 3
+# a = input("Enter a number: ")
+# a = int(a)
+# b = input("Enter another number: ")
+# b = int(b)
+# result = (a + b) * (a + b)
+# print("The result is: ", result)
+
+# Task 4
+# feet = input("Enter a length in feet: ")
+# feet = int(feet)
+# iches = input("Enter a length in inches: ")
+# iches = int(iches)
+# sm = (feet * 30.48) + (iches * 2.54)
+# print("The length in centimeters is: ", round(sm))
+
+# Task 5
+# feet = input("Enter a length in feet: ")
+# feet = int(feet)
+# iches = feet * 12
+# yards = feet / 3
+# miles = feet / 5280
+# print("The length in inches is: ", iches)
+# print("The length in yards is: ", yards)
+# print("The length in miles is: ", miles)
+
+# Task 6
+# symbol = input("Enter a symbol: ")
+# symbol = ord(symbol)
+# print("The ASCII code of the symbol is: ", symbol)
+
+# Task 7
+# symbol = input("Enter a symbol: ")
+# symbol = int(symbol)
+# symbol = chr(symbol)
+# print("The character represented by the ASCII code is: ", symbol)
+
+# Task 8
+# a = input("Enter a number: ")
+# a = int(a)
+# b = input("Enter another number: ")
+# b = int(b)
+# print (f"{a} + {b} = {a+b}")
+
+# Task 9
+# a = input("Enter a number: ")
+# a = int(a)
+# b = input("Enter another number: ")
+# b = int(b)
+# a, b = b, a
+# print(a)
+# print(b)
+
+# Task 10
+# number = input("Enter a number: ")
+# number = round(float(number), 2)
+# print (number)
+
+# python variables/lesson_02.py

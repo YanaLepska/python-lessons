7c204963ee8321c117409af18a07bcff66caab04
Yana Lepska <lepskayana1624@gmail.com>
2026-04-27 17:24:05 +0300

    Create lesson_04.py

---
+++ variables/lesson_04.py
@@ -0,0 +1,52 @@
+#Task 1
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# product = a * b
+# summ = a + b
+# quotient=a / b
+# difference=a - b
+# power=a ** b
+# print(product, summ, quotient, difference, power,sep="&")
+
+
+# Task 2
+a = int(input("Enter a number: "))
+print(f"{a:*<10d}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python variables/lesson_04.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python variables/lesson_04.py

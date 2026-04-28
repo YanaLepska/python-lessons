f6fb3b78f2c362d51139736a4df26b33c49de82a
Yana Lepska <lepskayana1624@gmail.com>
2026-04-28 13:41:07 +0300

    Update lesson_04.py
    Exercises 1-11. Variables and data types.

--- variables/lesson_04.py
+++ variables/lesson_04.py
@@ -10,18 +10,70 @@
 
 
 # Task 2
-a = int(input("Enter a number: "))
-print(f"{a:*<10d}")
+# a = float(input("Enter a number: "))
+# b = float(input("Enter another number: "))
+# c= a/b
+# d=b/a
+# print(f"{c:9.5f}***{d:9.5f}")
 
+# Task 3
+# a = int(input("Enter a number: "))
+# b = float(input("Enter another number: "))
+# c = input("Enter a word: ")
+# print(type(a), type(b), type(c))
 
+# Task 4
+# a = input("Enter a number: ")
+# a = float(a)
+# print(f"{a:.3f}")
 
+# Task 5
+# a = input("Enter a number: ")
+# try:
+#     a = int(a)
+#     print(True)
+# except ValueError:
+#     print(False)
 
+# Task 6
+# a = int(input("Enter a full price: "))
+# b = float(input("Enter a discount: "))
+# c = a - (b*a)
+# print(f"The discounted price is: {c}")
 
+# Task 7
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))    
+# c =int(input("Enter one more number: "))
+# print(max(a, b, c))
+# print(min(a, b, c))
 
+# Task 8
+# hours = int(input("Enter hours: "))
+# break_minutes = int(input("Enter minutes: "))
+# slepp_minutes = hours*60+break_minutes
+# print(f"The total number of minutes is: {slepp_minutes}")
 
+# Task 9
+# total_bill = float(input("Enter a bill amount: "))
+# tip = float(input("Enter a tip percentage: "))
+# piece_bill = (total_bill+(total_bill*tip/100))/3
+# print(f"Each person should pay: {piece_bill:.2f}")
 
+# Task 10
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# x = int(input("Enter one more number: "))
+# y = ((a/b)**x)+(a**(x+1))/b**x
+# print(abs(y))
 
-
+# Task 11
+students= int(input("Enter the number of students: "))
+apples= int(input("Enter the number of apples: "))
+apples_per_student = apples//students
+apples_left = apples%students
+print (apples_per_student)
+print (apples_left)
 
 
 

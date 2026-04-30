2b1f8b77675418421ab3519c4b7c1b55fa6090c0
Yana Lepska <lepskayana1624@gmail.com>
2026-04-30 15:01:03 +0300

    Create lesson_06.py
    Exercises

---
+++ variables/lesson_06.py
@@ -0,0 +1,63 @@
+# Task 1
+# a = int(input("Enter a number: "))
+# a = 1 - a
+# print(a)
+
+# Task 2
+# number = input("Enter a number: ")
+# number = number*100
+# number = int(number)**2
+# print(number)
+
+# Task 3
+# n = input("Enter a number: ")
+# n = int(n)
+# sum = (n * (n + 1)) // 2
+# print(sum)
+
+# Task 4
+# number = input("Enter a number: ")
+# number = int(number)
+# binary = format(number, '08b')
+# print(binary)
+
+# Task 5
+# number = int(input("Enter a number: "))
+# hexadecimal = format(number, 'x')
+# print(hexadecimal)
+
+# Task 6
+# number = int(input("Enter a number: "))
+# number = number**10
+# number = str(number)*4
+# result = int(number)**(1/10)
+# print(result)
+
+# Task 7
+# a= float(input("Enter a number: "))
+# a = a+0.5
+# print(int(a))
+
+# Task 8
+# days = int(input("Enter the number of days: "))
+# hours = days * 24
+# minutes = hours * 60
+# seconds = minutes * 60
+# print(f"{hours:<10}hours")
+# print(f"{minutes:<10}minutes")
+# print(f"{seconds:<10}seconds")
+
+# Task 9
+# number = input("Enter a number: ")
+# number = number[::-1]
+# print(number)
+
+# Task 10
+# first_class = int(input("Enter the number of students in the first class: "))
+# second_class = int(input("Enter the number of students in the second class: "))
+# third_class = int(input("Enter the number of students in the third class: "))
+# total_students = first_class + second_class + third_class
+# desks_needed = (total_students + 1) // 2
+# print(desks_needed)
+
+# python variables/lesson_06.py

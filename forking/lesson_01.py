30bf1cac21c13d02d116603b67d76893d6c6a5d1
Yana Lepska <lepskayana1624@gmail.com>
2026-05-05 11:10:01 +0300

    Create lesson_01.py
    Forking topic

---
+++ forking/lesson_01.py
@@ -0,0 +1,100 @@
+# 1
+# password = 'starlink'
+# user_password = input('Enter password: ')
+# if user_password == password:
+#     print('Password accepted')
+# else:
+#     print('Invalid password')
+
+# 2
+# name_1 = input('Enter your name: ')
+# name_2 = input('Enter your name again: ')
+# if name_1 < name_2:
+#     print(name_1)
+#     print(name_2)
+# else:
+#     print(name_2)
+#     print(name_1)
+
+# 3
+# n = int(input('Enter a number: '))
+# if n==1:
+#     print('One')
+# elif n==2:
+#     print('Two')
+# elif n==3:
+#     print('Three')
+# else:
+#     print('Unknown number')
+
+# 4
+# symbol_1 = input('Enter a symbol: ')
+# symbol_2 = input('Enter another symbol: ')
+# if symbol_1 < symbol_2:
+#     print(f"{symbol_1} comes before {symbol_2}")
+# elif symbol_1 > symbol_2:
+#     print(f"{symbol_2} comes before {symbol_1}")
+
+# 5
+# speed_1 = int(input('Enter the speed of the first car km/h: '))
+# speed_2 = int(input('Enter the speed of the second car m/s: '))
+# speed_1 = speed_1 * 10/36
+# if speed_1 > speed_2:
+#     print(speed_1)
+# elif speed_1 < speed_2:
+#     print(speed_2)
+
+# 6
+# radius = float(input('Enter the radius of the circle: '))
+# pi = 3.14
+# area = pi * radius ** 2
+# side = float(input('Enter the side of the square: '))
+# area_square = side ** 2
+# if area > area_square:
+#     print('The circle has a larger area')
+# else:    print('The square has a larger area')
+
+# 7
+# m1 = float(input('Enter the mass of the first object in kg: '))
+# m2 = float(input('Enter the mass of the second object in kg: '))
+# v1 = float(input('Enter the velocity of the first object in m/s: '))
+# v2 = float(input('Enter the velocity of the second object in m/s: '))
+# ro1 = m1/v1
+# ro2 = m2/v2
+# if ro1 > ro2:
+#     print(m1, v1)
+# else:
+#     print(m2, v2)
+
+# 8
+# word = input('Enter a yes or no: ')
+# if word == 'yes' or word == 'Yes' or word == 'YES':
+#     print(' Yes')
+# else:
+#     print(' No')
+
+# 9
+# s = int(input('Enter a number: '))
+# t = int(input('Enter time in seconds: '))
+# v = (s/t)*60
+# if v > 60:
+#     print("Traffic rules are not met.")
+# else:    print("Traffic rules are executed.")
+
+# 10
+# a = 3
+# b = -5
+# c = 7
+# d = 0
+# e = -1
+# positive_count = 0
+# for i in [a, b, c, d, e]:
+#     if i > 0:
+#         positive_count += 1
+# print(positive_count)
+
+
+
+
+
+# python forking/lesson_01.py

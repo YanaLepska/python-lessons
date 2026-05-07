eb5e8fd68b4480229a8579a1a745620611fd0232
Yana Lepska <lepskayana1624@gmail.com>
2026-05-07 09:58:47 +0300

    Update lesson_03.py
    Exercise 151

--- forking/lesson_03.py
+++ forking/lesson_03.py
@@ -1,20 +1,121 @@
+# 1
+# volume = int(input("Enter a volume: "))
+# delta = int(input("Enter a delta %: "))
+# distance = int(input("Enter a distance: "))
+# total_distance = 200
+# v1 = (volume*delta)/100
+# s1 = total_distance/distance
+# v2 = v1*distance
+# if v2 < s1:
+#     print(f"You can drive another {v2} kilometers. \n Next refueling after 200 kilometers. \n FILL IN NOW!")
+# else:
+#     print(f"You can drive another {v2} kilometers.")
+
+# 2
+# n = input("Enter a number:")
+# a = n[-1]
+# print(a)
+# if a == '5':
+#     print('True')
+# else:
+#     print('False')
+
+# 3
+# n = int(input("Enter a number:"))
+# if n in range(-5, 3):
+#     print(True)
+# else:
+#     print(False)
+
+# 4
+# a = int(input("Enter a number:"))
+# b = int(input("Enter a number 2:"))
+# if a%b==0 or b%a==0:
+#     print(True)
+# else:
+#     print(False)
+
+# 5
+# bytes = int(input("Enter MB:"))
+# n= bytes*(1024**2)
+# print(n, 'B')
+# m = bytes*1024
+# print(m, 'KB')
+
+# 6
+# n = int(input())
+# if n == 0:
+#     print("green")
+
+# elif 1 <= n <= 10:
+#     if n % 2 == 0:
+#         print("black")
+#     else:
+#         print("red")
+
+# elif 11 <= n <= 18:
+#     if n % 2 == 0:
+#         print("red")
+#     else:
+#         print("black")
+
+# elif 19 <= n <= 28:
+#     if n % 2 == 0:
+#         print("black")
+#     else:
+#         print("red")
+
+# elif 29 <= n <= 36:
+#     if n % 2 == 0:
+#         print("red")
+#     else:
+#         print("black")
+
+# else:
+#     print("The bet will not play!")
+
+# 7
+# x1 = int(input('Enter x1: '))
+# y1 = int(input('Enter y1: '))
+# x2 = int(input('Enter x2: '))
+# y2 = int(input('Enter y2: '))
+# if x1 > x2 and y1 > y2:
+#     print(x1, y1)
+# if x1 == x2 and y1 == y2:
+#     print('The distance is the same')
+# else:
+#     print(x2, y2)
+
+# 8
+# x1 = int(input('Enter x1: '))
+# y1 = int(input('Enter y1: '))
+# r = int(input('Enter r: '))
+# c= (x1**2)+(y1**2)
+# if c == r**2:
+#     print('The point belongs to the circle')
+# else:
+#     print('The point is outside the circle')
+
+# 9
+# x = int(input('Enter x: '))
+# a = x//100
+# b = (x%100)//10
+# c = x%100%10
+# s1 = (a+b+c)**2
+# s2 = (a**3)+(b**3)+(c**3)
+# if s1==s2:
+#     print(True)
+# else:
+#     print(False)
+
+# 10
+# k = int(input())
+# if k % 7 == 6 or k % 7 == 0:
+#     print("Weekend")
+# else:
+#     print("Workday")
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# python forking/lesson_02.py
+# python forking/lesson_03.py
